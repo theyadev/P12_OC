502ea17d96bac5032115109c19b2beeb847a64de
Théo FASSI <tfa@subteno.com>
2022-11-24 09:48:12 +0100

    feature: added model ContractStatus

--- crm/admin.py
+++ crm/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from .models.Client import Client
 from .models.Contract import Contract
+from .models.ContractStatus import ContractStatus
 from .models.Event import Event
 
 admin.site.register(Client)
 admin.site.register(Contract)
+admin.site.register(ContractStatus)
 admin.site.register(Event)
-
-
-
-
-
-# Register your models here.
--- crm/models/__init__.py
+++ crm/models/__init__.py
@@ -1,3 +1,4 @@
 from . import Client
 from . import Contract
+from . import ContractStatus
 from . import Event
