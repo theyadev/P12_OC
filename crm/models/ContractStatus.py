5a02782fa470a286ea8d9870d3ee9df1d4b58371
Théo FASSI <tfa@subteno.com>
2022-11-24 10:23:05 +0100

    chore: added plural

--- crm/models/ContractStatus.py
+++ crm/models/ContractStatus.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class ContractStatus(models.Model):
+    class Meta:
+        verbose_name_plural = "Contract Statuses"
+
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
